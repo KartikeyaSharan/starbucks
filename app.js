dd712d3e7c430bfbb800c8684e110c655e1f3e81
unknown <karan.2.10.1993@gmail.com>
2020-12-05 21:24:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function imgSlider(source)
+{
+    document.querySelector('.starbucks').src = source;
+}
+
+function changeCircleColor(color)
+{
+    document.querySelector('.circle').style.background = color;
+}
